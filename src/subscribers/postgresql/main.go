be2310600d72d7429a56381452575ca83af684f3
Colant Alan <19172637+alancolant@users.noreply.github.com>
2023-07-24 12:47:10 +0200

    fix string

--- src/subscribers/postgresql/main.go
+++ src/subscribers/postgresql/main.go
@@ -365,7 +365,6 @@ func (pg *Subscriber) GetFullRecordsForRelationUpdate(relationUpdates types.Rela
 				wheresRelationRaw = "AND " + wheresRelationRaw
 				sqlQuery = index.GetSelectQuery() + " " + wheresSqlRaw + " " + wheresRelationRaw
 			}
-			fmt.Println(sqlQuery)
 			//fmt.Println(sqlQuery)
 			for row := range pg.getQueryRecords(sqlQuery, idx, true) {
 				ch <- row
